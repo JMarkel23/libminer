f89eed68c76630d2932a2840b355a12abd829bd5
JMarkel23 <justinm@ping.com>
2023-09-17 14:13:58 -0700

    Upddate tests for new sizes argument

--- tests/testthat/test-lib-summary.R
+++ tests/testthat/test-lib-summary.R
@@ -1,4 +1,6 @@
 test_that("lib_summary returns expected results", {
+#Check conditions up front, so it fails early
+
   res <- lib_summary() #Call lib_summary within the test
   expect_s3_class(res, 'data.frame')
   expect_equal(ncol(res), 2)
@@ -8,6 +10,15 @@ test_that("lib_summary returns expected results", {
 })
 
 test_that("lib_summary fails appropriately", {
-  expect_error(lib_summary("foo"), "unused argument")
+  expect_error(lib_summary("foo"))
+
+})
+
+test_that("sizes argument works", {
+ res <- lib_summary(sizes = TRUE)
+ expect_equal(ncol(res), 3)
+ expect_equal(names(res), c("Library", "n_packages", "lib_size"))
+ expect_type(res$lib_size, "double")
+
 
 })
